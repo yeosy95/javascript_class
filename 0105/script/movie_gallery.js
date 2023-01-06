637d483440a9b3f31ed72cf725fea7004d49c76f
yeosy95 <yeosy95@gmail.com>
2023-01-06 14:36:05 +0900

    js complete

--- 0105/script/movie_gallery.js
+++ 0105/script/movie_gallery.js
@@ -5,15 +5,15 @@ const img_list = document.querySelectorAll('.lnb > li');
 const left_btn = document.querySelector('i.fa-angle-left');
 const right_btn = document.querySelector('i.fa-angle-right');
 
-document.getElementById('page').innerHTML=n + ' / 16';
+document.getElementById('page').innerHTML = n + ' / 16';
 
 img_list.forEach((el, index) => {
   el.onclick=()=>{
     console.log(index);
     n = index+1; //인덱스번호에 1을 더하여 1부터 숫자가 나오게...
 
-    document.getElementById('page').innerHTML=n + ' / 16';
-    document.getElementById('photo').src='./images/movie_image'+n+'.jpg';
+    document.getElementById('page').innerHTML = n + ' / 16';
+    document.getElementById('photo').src='./images/movie_image'+ n +'.jpg';
   }
 });
 
